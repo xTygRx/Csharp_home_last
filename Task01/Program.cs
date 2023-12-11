38f7320acaa3f7cb3d322d97a1a7a2bf79ea66db
xTygRx <divinged@inbox.ru>
2023-12-11 12:51:51 +0300

    Добавлено решение 1 задачи

---
+++ Task01/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+//Использовать рекурсию, не использовать циклы.
+
+
+int ReadInt(string str)
+{
+    System.Console.Write(str);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void PrintNumbers(int M, int N)
+{
+    if(M < N) return;
+    PrintNumbers(M - 1, N);
+    System.Console.Write(M + " ");
+}
+
+// ------------------
+int M = ReadInt("Введите число M: ");
+
+int N = ReadInt("Введите число N: ");
+
+System.Console.WriteLine("На отрезке расположены следующие числа");
+if (M > N) PrintNumbers(M, N);
+else PrintNumbers(N, M);
